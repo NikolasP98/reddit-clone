2d297c34244fdbb8bbdc90686d12d8d8802c2156
Nikolas Pinon <nikolas.pinon98@gmail.com>
2022-01-20 13:31:27 -0700

    added necessary post and auth components

--- client/src/views/Home/index.jsx
+++ client/src/views/Home/index.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import axios from 'axios';
 
 const Home = () => {
-	return <div>Home</div>;
+	const { data } = axios
+		.get('http://localhost:5000/api/v1/user')
+		.then((res) => {
+			console.log(res.data);
+		});
+
+	return (
+		<div>
+			<nav>navigator</nav>
+			<h1>Home</h1>
+			<p>All Posts</p>
+		</div>
+	);
 };
 
 export default Home;
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+	plugins: [react()],
+	resolve: {
+		alias: {
+			components: path.resolve(__dirname, './src/components'),
+			views: path.resolve(__dirname, './src/components/views'),
+			// utils: path.resolve(__dirname, './src/utils'),
+			// assets: path.resolve(__dirname, './src/assets'),
+		},
+	},
+});
